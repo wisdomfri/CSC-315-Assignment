662d8617c0df79c1e990da0b12ba92766bf63330
wisdomfri <156893861+wisdomfri@users.noreply.github.com>
2025-12-08 00:06:40 +0100

    Remove main function and MyApp class

--- main[1].dart
+++ main[1].dart
@@ -1,23 +1,2 @@
-import 'package:csc_315/screens/login_screen.dart';
-import 'package:flutter/material.dart';
 
 
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepGreen),
-      ),
-      home: const Login(),
-    );
-  }
-}
